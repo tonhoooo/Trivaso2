9a99b2498febc1a1bd8fd0d8cbf8d69086da4b55
n <jonascr86@gmail.com>
2017-06-12 21:49:58 -0300

    Implementação Login google.

---
+++ app/src/main/java/com/example/zucho/trivaso/ContaFragment.java
@@ -0,0 +1,26 @@
+package com.example.zucho.trivaso;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jonas on 12/06/17.
+ */
+
+public class ContaFragment extends Fragment {
+
+    public ContaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_conta, container, false);
+    }
+}
